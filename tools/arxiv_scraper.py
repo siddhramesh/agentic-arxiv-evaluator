ba3de4794e81e94e55619e1a2f681e7692a40f46
siddhramesh <siddarameshdiksangi@gmail.com>
2026-03-10 23:26:55 +0530

    Update arxiv_scraper.py

--- tools/arxiv_scraper.py
+++ tools/arxiv_scraper.py
@@ -1,14 +1,23 @@
 import arxiv
+import requests
 
 def fetch_paper(url):
 
     paper_id = url.split("/")[-1]
 
     search = arxiv.Search(id_list=[paper_id])
-
     paper = next(search.results())
 
-    return {
+    abstract = paper.summary
+
+    # Basic section placeholders (since arxiv API gives only abstract)
+    # Agents will simulate analysis based on abstract
+    paper_data = {
         "title": paper.title,
-        "abstract": paper.summary
+        "abstract": abstract,
+        "methodology": abstract,
+        "results": abstract,
+        "conclusion": abstract
     }
+
+    return paper_data
