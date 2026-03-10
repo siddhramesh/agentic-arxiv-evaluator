e48cae9b7e7d766d6741283dc697d38f31a6a1a7
siddhramesh <siddarameshdiksangi@gmail.com>
2026-03-10 21:36:49 +0530

    Update report_formatter.py

--- report/report_formatter.py
+++ report/report_formatter.py
@@ -6,7 +6,25 @@ def format_report(title, evaluation):
 ## Paper Title
 {title}
 
-{evaluation}
+## Executive Summary
+{evaluation['executive_summary']}
+
+## Detailed Scores
+
+**Consistency Score:** {evaluation['consistency_score']} / 100
+
+**Grammar Rating:** {evaluation['grammar_rating']}
+
+**Novelty Index**
+{evaluation['novelty_index']}
+
+**Accuracy / Fabrication Risk**
+{evaluation['fabrication_risk']}
+
+## Fact Check Log
 """
+
+    for item in evaluation["fact_check_log"]:
+        report += f"- {item}\n"
 
     return report
