8a69445e97eb054717b105bff4f5d0626949dec6
Siddhramesh Diksanggi <siddarameshdiksangi@gmail.com>
2026-03-11 16:23:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-# SQLite fix for ChromaDB on Streamlit Cloud
-try:
-    __import__('pysqlite3')
-    import sys
-    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-except ImportError:
-    pass
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 import os
