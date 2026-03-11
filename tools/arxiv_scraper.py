f0c8d44d2826341e9cdac7564fa2dbc480accc0a
Siddhramesh Diksanggi <siddarameshdiksangi@gmail.com>
2026-03-11 13:54:08 +0530

    Update arxiv_scraper.py

--- tools/arxiv_scraper.py
+++ tools/arxiv_scraper.py
@@ -1,14 +1,20 @@
 import requests
-import fitz
+
 
 def download_pdf(arxiv_url):
 
     paper_id = arxiv_url.split("/")[-1]
+
     pdf_url = f"https://arxiv.org/pdf/{paper_id}.pdf"
 
     response = requests.get(pdf_url)
 
-    with open("paper.pdf", "wb") as f:
+    if response.status_code != 200:
+        raise Exception("Failed to download paper from arXiv.")
+
+    file_path = "arxiv_paper.pdf"
+
+    with open(file_path, "wb") as f:
         f.write(response.content)
 
-    return "paper.pdf"
+    return file_path
